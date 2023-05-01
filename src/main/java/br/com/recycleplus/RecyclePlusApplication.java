249269ab039159814c4b9f7ca7c4bf4ec28212ed
JaelsonJonas <rm93915@fiap.com.br>
2023-05-01 13:59:13 -0300

    Incluindo o boilerplate do Spring Initializr

---
+++ src/main/java/br/com/recycleplus/RecyclePlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.recycleplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclePlusApplication.class, args);
+	}
+
+}
